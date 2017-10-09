4d13e64c973c877784f7accfea8588f9594dc0b4
Sharath Bhat <sharubhat@gmail.com>
2017-10-09 09:02:06 -0700

    Added a few comments to LRU Cache solution

--- src/main/java/com/leetcode/ds/hard/P146LruCache.java
+++ src/main/java/com/leetcode/ds/hard/P146LruCache.java
@@ -3,18 +3,29 @@ package com.leetcode.ds.hard;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * https://leetcode.com/problems/lru-cache/description/
+ * Let's consider a constant stream of cache requests with a cache capacity of 3,
+ * see below:
+ * A, B, C, A, A, A, A, A, A, A, A, A, A, A, B, C, D
+ * If we just consider a Least Recently Used (LRU) cache with
+ * a HashMap + doubly linked list implementation with
+ * O(1) eviction time and O(1) load time, we would have  the following elements
+ * cached while processing the caching requests as mentioned above.
+ * [A]
+ * [A, B]
+ * [A, B, C]
+ * [B, C, A] <- a stream of As keeps A at the head of the list.
+ * [C, A, B]
+ * [A, B, C]
+ * [B, C, D] <- here, we evict A
+ */
 public class P146LruCache {
-  /**
-   * A version of doubly linked list tailored to LRU Cache.
-   */
   private static class Node {
     int key;
     int value;
     Node prev;
     Node next;
-
-    public Node() {
-    }
 
     public Node(int key, int value) {
       this.key = key;
@@ -28,6 +39,8 @@ public class P146LruCache {
       next.prev = prev;
     }
 
+    // add to left of head is equivalent to adding to end of list
+    // if the list were singly linked
     void addToLeft(Node left) {
       left.next = this;
       left.prev = prev;
@@ -87,5 +100,18 @@ public class P146LruCache {
       cache.put(key, node);
     }
     head.addToLeft(node);
+  }
+
+  public static void main(String[] args) {
+    P146LruCache p = new P146LruCache(2);
+    p.put(1,1);
+    p.put(2,2);
+    p.get(1);
+    p.put(3,3);
+    p.get(2);
+    p.put(4,4);
+    p.get(1);
+    p.get(3);
+    p.get(4);
   }
 }
