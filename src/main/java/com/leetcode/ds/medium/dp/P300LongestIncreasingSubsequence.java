72c229b2a4dbd2c724f22b8d632a6d6331a03ee5
Sharath Bhat <sharubhat@gmail.com>
2017-10-12 09:42:19 -0700

    Added some clarifications and examples to longest increasing subsequence

--- src/main/java/com/leetcode/ds/medium/dp/P300LongestIncreasingSubsequence.java
+++ src/main/java/com/leetcode/ds/medium/dp/P300LongestIncreasingSubsequence.java
@@ -8,10 +8,10 @@ import java.util.Arrays;
 /* Approach :
  *
  * Optimal Substructure:
- * LIS(i) -> Length of longest increasing subsequence which includes A[i]as it's last
+ * UP(i) -> Length of longest increasing subsequence which includes A[i] as it's last
  * element.
- * LIS(i) = 1 + Max(LIS(j) where j < i such that A[j] < A[i])
- * LIS(i) = 1 if no such j exists
+ * UP(i) = 1 + Max(UP(j) where j < i such that A[j] < A[i])
+ * UP(i) = 1 if no such j exists
  *
  * Overlapping Subproblems:
  * If we solve using recursion for calculating the solution for jth index, we will be
@@ -20,10 +20,10 @@ import java.util.Arrays;
  *
  * Example:
  * A[] = {2,4,1,6}
- * i = 1, LIS(1) = 1
- * i = 2, LIS(2) = 1 + Max(LIS(1)) = 1 + 1 = 2 (4 > 2)
- * i = 3, LIS(3) = 1 (1 < 3, 1 < 4)
- * i = 4, LIS(4) = 1 + Max(LIS(1), LIS(2), LIS(3)) = 1 + 2 = 3 (6 > 2, 6 > 4, 6 > 1)
+ * i = 1, UP(1) = 1
+ * i = 2, UP(2) = 1 + Max(UP(1)) = 1 + 1 = 2 (4 > 2)
+ * i = 3, UP(3) = 1 (1 < 3, 1 < 4)
+ * i = 4, UP(4) = 1 + Max(UP(1), UP(2), UP(3)) = 1 + 2 = 3 (6 > 2, 6 > 4, 6 > 1)
  *
  * NOTE: To print the Actual elements -
  * find the index which contains the longest sequence, print that index from main array.
@@ -34,37 +34,46 @@ import java.util.Arrays;
  */
 public class P300LongestIncreasingSubsequence {
   public void findSubSequence(int[] arr) {
-    int[] lis = new int[arr.length];
+    int[] up = new int[arr.length];
     for (int i = 0; i < arr.length; i++) {
       int max = -1;
       for (int j = 0; j < i; j++) {
         if (arr[j] < arr[i]) {
-          if (max == -1 || max < lis[j] + 1) {
-            max = 1 + lis[j];
+          if (max == -1 || max < up[j] + 1) {
+            max = 1 + up[j];
           }
         }
       }
-      lis[i] = (max == -1) ? 1 : max; // (max == -1) means none of arr[j] < arr[i]
+      if(max == -1) { // (max == -1) means none of arr[j] < arr[i]. So start new seq.
+        max = 1;
+      }
+      up[i] = max;
     }
-    // find max in the lis[]
-    int result  = -1;
-    int index = -1;
+    // up[] or length of longest increasing subsequence at each index
+    // in current example at this stage : [1, 2, 2, 1, 3, 3, 4, 4, 5, 6, 7, 2]
+
+    // find max in the up[]
+    int lengthLongest  = -1;
+    int lastIndex = -1;
     for (int i = 0; i < arr.length; i++) {
-      if (result < lis[i]) {
-        result = lis[i];
-        index = i;
+      if (lengthLongest < up[i]) {
+        lengthLongest = up[i];
+        lastIndex = i;
       }
     }
     // start moving backwards from the end
-    int res = result;
+    int res = lengthLongest;
     int[] subseq = new int[res];
-    for (int i = index; i >= 0; i--) {
-      if (lis[i] == res) {
+    for (int i = lastIndex; i >= 0; i--) {
+      // there can be more than one element which is capable of being part of
+      // longest subsequence, however we just need one.
+      // e.g 3,3,4,4 in current example up[]
+      if (up[i] == res) {
         subseq[--res] = arr[i];
       }
     }
 
-    System.out.println(result);
+    System.out.println(lengthLongest);
     System.out.println(Arrays.toString(subseq));
   }
 
