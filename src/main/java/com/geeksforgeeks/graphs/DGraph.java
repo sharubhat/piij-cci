8ded35c81000a755c4ff0fe3e7b8c6d2e7ec84b0
Sharath Bhat <sharubhat@gmail.com>
2017-10-16 17:03:10 -0700

    Added comments

--- src/main/java/com/geeksforgeeks/graphs/DGraph.java
+++ src/main/java/com/geeksforgeeks/graphs/DGraph.java
@@ -3,21 +3,23 @@ package com.geeksforgeeks.graphs;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Stack;
 
 /**
  * Adjacency list representation of undirected graph.
  * There are many ways to represent a graph. Current implementation uses an array of
- * LinkedLists. Alternative is to use a HashMap<Integer, List<Integer>> which under the
+ * LinkedLists. Alternative is to use a map of Integer -> List of Integers, which under the
  * hood does the same thing.
+ * Note: Generally m denotes number of vertices and n denotes number of edges.
  */
 public class DGraph {
-  int v;
+  int numVertices;
   LinkedList<Integer>[] adjListArray;
 
-  DGraph(int v) {
-    this.v = v;
-    this.adjListArray = new LinkedList[v];
-    for (int i = 0; i < v; i++) {
+  DGraph(int n) {
+    this.numVertices = n;
+    this.adjListArray = new LinkedList[n];
+    for (int i = 0; i < n; i++) {
       adjListArray[i] = new LinkedList<>();
     }
   }
@@ -36,17 +38,17 @@ public class DGraph {
   }
 
   void bfs(int s) {
-    boolean[] visited = new boolean[v];
+    boolean[] visited = new boolean[this.numVertices];
     LinkedList<Integer> queue = new LinkedList<>();
     queue.add(s);
     visited[s] = true;
-    while(queue.size() != 0) {
+    while (queue.size() != 0) {
       s = queue.poll();
       actOnVertex(s);
       Iterator<Integer> it = adjListArray[s].iterator();
-      while(it.hasNext()) {
+      while (it.hasNext()) {
         int n = it.next();
-        if(!visited[n]) {
+        if (!visited[n]) {
           visited[n] = true;
           queue.add(n);
         }
@@ -55,7 +57,7 @@ public class DGraph {
   }
 
   void dfs(int s) {
-    dfs(s, new boolean[this.v]);
+    dfs(s, new boolean[this.numVertices]);
   }
 
   private void dfs(int s, boolean[] visited) {
@@ -64,7 +66,7 @@ public class DGraph {
     Iterator<Integer> it = adjListArray[s].iterator();
     while (it.hasNext()) {
       int n = it.next();
-      if(!visited[n]) {
+      if (!visited[n]) {
         dfs(n, visited);
       }
     }
@@ -72,11 +74,12 @@ public class DGraph {
 
   /**
    * Detects presence of cycles in a directed graph.
+   *
    * @return boolean
    */
   public boolean hasCycle() {
-    boolean[] visited = new boolean[v];
-    for (int i = 0; i < v; i++) {
+    boolean[] visited = new boolean[this.numVertices];
+    for (int i = 0; i < this.numVertices; i++) {
       if (!visited[i] && hasCycle(i, visited, new HashSet<>())) {
         return true;
       }
@@ -86,8 +89,9 @@ public class DGraph {
 
   /**
    * Recursive helper for finding cycles.
-   * @param s starting vertex.
-   * @param visited boolean array to keep track of visited vertices.
+   *
+   * @param s                  starting vertex.
+   * @param visited            boolean array to keep track of visited vertices.
    * @param verticesOnRecStack HashSet to keep track of vertices in current path.
    * @return boolean
    */
@@ -95,13 +99,13 @@ public class DGraph {
     visited[s] = true;
     verticesOnRecStack.add(s);
     Iterator<Integer> adjIt = this.adjListArray[s].iterator();
-    while(adjIt.hasNext()) {
+    while (adjIt.hasNext()) {
       int n = adjIt.next();
       if (verticesOnRecStack.contains(n)) {
         return true;
       }
-      if(!visited[n]) {
-        if(hasCycle(n, visited, verticesOnRecStack)) {
+      if (!visited[n]) {
+        if (hasCycle(n, visited, verticesOnRecStack)) {
           return true;
         }
       }
@@ -110,16 +114,23 @@ public class DGraph {
     return false;
   }
 
+  /**
+   * Finds out if there is a path between two nodes of a directed graph.
+   *
+   * @param s starting vertex
+   * @param e destination vertex
+   * @return boolean
+   */
   public boolean pathExists(int s, int e) {
-    return pathExists(s, e, new boolean[v]);
+    return pathExists(s, e, new boolean[this.numVertices]);
   }
 
   private boolean pathExists(int s, int e, boolean[] visited) {
     visited[s] = true;
-    if(s == e) {
+    if (s == e) {
       return true;
     }
-    Iterator<Integer> adjIt = adjListArray[s].iterator();
+    Iterator<Integer> adjIt = this.adjListArray[s].iterator();
     while (adjIt.hasNext()) {
       int n = adjIt.next();
       if (!visited[n]) {
@@ -129,10 +140,40 @@ public class DGraph {
     return false;
   }
 
+  /**
+   * Prints one of the possible topological sort of vertices.
+   */
+  public void topologicalSort() {
+    Stack stack = new Stack();
+    boolean[] visited = new boolean[this.numVertices];
+
+    for (int i = 0; i < this.numVertices; i++) {
+      if (!visited[i]) {
+        topologicalSort(i, visited, stack);
+      }
+    }
+
+    while (!stack.isEmpty()) {
+      System.out.print(stack.pop() + " ");
+    }
+  }
+
+  private void topologicalSort(int s, boolean[] visited, Stack stack) {
+    visited[s] = true;
+    Iterator<Integer> adjIt = this.adjListArray[s].iterator();
+    while (adjIt.hasNext()) {
+      int n = adjIt.next();
+      if(!visited[n]) {
+        topologicalSort(n, visited, stack);
+      }
+    }
+    stack.push(s);
+  }
+
   @Override
   public String toString() {
     StringBuffer sb = new StringBuffer();
-    for (int i = 0; i < this.v; i++) {
+    for (int i = 0; i < this.numVertices; i++) {
       sb.append("Adjacency list of vertex ").append(i).append(" : ");
       sb.append(this.adjListArray[i]);
       sb.append("\n");
@@ -140,8 +181,11 @@ public class DGraph {
     return sb.toString();
   }
 
-  public static void main(String args[])
-  {
+  /**
+   * A bunch of small tests to verify the basic functionalities.
+   * @param args input args
+   */
+  public static void main(String[] args) {
     DGraph g = new DGraph(4);
 
     g.addEdge(0, 1);
@@ -151,8 +195,8 @@ public class DGraph {
     g.addEdge(2, 3);
     g.addEdge(3, 3);
 
-    System.out.println("Following is Breadth First Traversal "+
-            "(starting from vertex 2)");
+    System.out.println("Following is Breadth First Traversal "
+            + "(starting from vertex 2)");
 
     System.out.println(g);
     g.bfs(2);
@@ -174,9 +218,24 @@ public class DGraph {
     cyclicDisconnected.addEdge(4, 5);
     cyclicDisconnected.addEdge(5, 6);
     cyclicDisconnected.addEdge(6, 4);
-    System.out.println("Cycle exists for cyclic disconnected graph : " + cyclicDisconnected.hasCycle());
-    System.out.printf("Path exists between %d and %d : %s\n", 1, 3, cyclicDisconnected.pathExists(1, 3));
-    System.out.printf("Path exists between %d and %d : %s\n", 1, 5, cyclicDisconnected.pathExists(1, 5));
+    System.out.println("Cycle exists for cyclic disconnected graph : "
+            + cyclicDisconnected.hasCycle());
+    System.out.printf("Path exists between %d and %d : %s\n",
+            1, 3, cyclicDisconnected.pathExists(1, 3));
+    System.out.printf("Path exists between %d and %d : %s\n",
+            1, 5, cyclicDisconnected.pathExists(1, 5));
+
+    // Create a graph given in the above diagram
+    DGraph topSortG = new DGraph(6);
+    topSortG.addEdge(5, 2);
+    topSortG.addEdge(5, 0);
+    topSortG.addEdge(4, 0);
+    topSortG.addEdge(4, 1);
+    topSortG.addEdge(2, 3);
+    topSortG.addEdge(3, 1);
+
+    System.out.println("Topological sort of the given graph : ");
+    topSortG.topologicalSort();
   }
 
 }
