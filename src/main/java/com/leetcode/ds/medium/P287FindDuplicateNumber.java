ce3d63fd7fe3d4c45ef08fe39e01eb2e820122fb
Sharath Bhat <sharubhat@gmail.com>
2017-10-28 23:20:49 -0700

    Cycle detection problems

---
+++ src/main/java/com/leetcode/ds/medium/P287FindDuplicateNumber.java
@@ -0,0 +1,28 @@
+package com.leetcode.ds.medium;
+
+/**
+ * https://leetcode.com/problems/find-the-duplicate-number/description/
+ *
+ * Check http://keithschwarz.com/interesting/code/?dir=find-duplicate for explanation
+ * https://en.wikipedia.org/wiki/Cycle_detection
+ */
+public class P287FindDuplicateNumber {
+  public int findDuplicate(int[] nums) {
+    int slow = 0;
+    int fast = 0;
+    int len = nums.length;
+    while (fast < len && nums[fast] < len) {
+      slow = nums[slow];
+      fast = nums[nums[fast]];
+      if(slow == fast) {
+        slow = 0;
+        while(slow != fast) {
+          slow = nums[slow];
+          fast = nums[fast];
+        }
+        return slow;
+      }
+    }
+    return -1;
+  }
+}
