b248991ef88c4ebd4cfc5d0fd062fbd085753dfc
Sharath Bhat <sharubhat@gmail.com>
2017-11-12 15:05:59 -0800

    Comments added

--- src/main/java/com/leetcode/ds/easy/P53MaximumSubarray.java
+++ src/main/java/com/leetcode/ds/easy/P53MaximumSubarray.java
@@ -1,10 +1,11 @@
 package com.leetcode.ds.easy;
 
 /**
- * Kaden's algorithm.
- * The max sum is initially 0. Suppose we solved the problem for A[1 .. i - 1],
- * A[i] is either part of the sub-array that produces max sum or is beginning of another sub-array that
- * might be producing max sum. So we keep track of max sum we have found so far and max ending at i.
+ * https://leetcode.com/problems/maximum-subarray/description/
+ * Kaden's algorithm. The max sum is initially 0. Suppose we solved the problem for A[1 .. i - 1],
+ * A[i] is either part of the sub-array that produces max sum or is beginning of another sub-array
+ * that might be producing max sum. So we keep track of max sum we have found so far and max ending
+ * at i.
  */
 public class P53MaximumSubarray {
   public int maxSubArray(int[] nums) {
@@ -13,7 +14,6 @@ public class P53MaximumSubarray {
     for (int i = 1; i < nums.length; i++) {
       maxEndingHere = Math.max(maxEndingHere + nums[i], nums[i]);
       maxSoFar = Math.max(maxSoFar, maxEndingHere);
-
     }
     return maxSoFar;
   }
