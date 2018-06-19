48c8c3f00394b12d6deecfa382c32f1640f2f4bd
Sharath Bhat <sharubhat@gmail.com>
2018-06-19 15:53:46 +0530

    Solved

--- src/main/java/topcoder/dp/MinNumCoins.java
+++ src/main/java/topcoder/dp/MinNumCoins.java
@@ -1,5 +1,7 @@
 package topcoder.dp;
 
+import java.util.Arrays;
+
 /**
  * Given a list of N coins, their values (V1, V2, … , VN), and the total sum S,
  * find the minimum number of coins the sum of which is S
@@ -7,5 +9,22 @@ package topcoder.dp;
  * or report that it’s not possible to select coins in such a way that they sum up to S.
  */
 public class MinNumCoins {
+  public int minCoinsRequired(int[] values, int sum) {
+    int[] min = new int[sum + 1];
+    Arrays.fill(min, Integer.MAX_VALUE);
+    min[0] = 0;
 
+    for (int i = 0; i < min.length; i++) {
+      for (int j = 0; j < values.length; j++) {
+        if (values[j] <= i && min[i - values[j]] + 1 < min[i]) {
+          min[i] = min[i - values[j]] + 1;
+        }
+      }
+    }
+    return min[sum];
+  }
+
+  public static void main(String[] args) {
+    System.out.println(new MinNumCoins().minCoinsRequired(new int[] {1, 3, 5}, 10));
+  }
 }
