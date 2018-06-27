2c66c787b4e547f94a69b0f4ae54545f3bd62a9e
Sharath Bhat <sharubhat@gmail.com>
2018-06-27 15:54:56 +0530

    Longest Nondecreasing Subsequence DP

--- src/main/java/topcoder/dp/LongestNonDecreasingSubsequence.java
+++ src/main/java/topcoder/dp/LongestNonDecreasingSubsequence.java
@@ -2,13 +2,71 @@ package topcoder.dp;
 
 import java.util.Arrays;
 
+/**
+ * There is a slight difference between longest increasing subsequence and length of longest
+ * non decreasing subsequence. The later counts integers that are equal.
+ *
+ * https://leetcode.com/problems/longest-increasing-subsequence/solution/#
+ */
 public class LongestNonDecreasingSubsequence {
-  public int lnds(int[] arr) {
+
+  // length of longest nondecreasing subsequence recursive
+  public int llndsRec(int[] arr) {
+    return llndsRec(arr, Integer.MIN_VALUE, 0);
+  }
+
+  private int llndsRec(int[] arr, int prev, int pos) {
+    if (pos == arr.length) {
+      return 0;
+    }
+    int taken = 0;
+    if (prev <= arr[pos]) {
+      taken = 1 + llndsRec(arr, arr[pos], pos + 1);
+    }
+    int notTaken = llndsRec(arr, prev, pos + 1);
+    return Math.max(taken, notTaken);
+  }
+
+  public int llndsMemo(int[] arr) {
+    int[][] memo = new int[arr.length + 1][arr.length];
+    for (int[] a : memo) {
+      Arrays.fill(a, -1);
+    }
+    return llndsMemo(arr, -1, 0, memo);
+  }
+
+  private int llndsMemo(int[] arr, int prevIndex, int pos, int[][] memo) {
+    if (pos == arr.length) {
+      return 0;
+    }
+    if (memo[prevIndex + 1][pos] >= 0) {
+      return memo[prevIndex + 1][pos];
+    }
+    int taken = 0;
+    if (prevIndex < 0 || arr[prevIndex] <= arr[pos]) {
+      taken = 1 + llndsMemo(arr, pos, pos + 1, memo);
+    }
+    int notTaken = llndsMemo(arr, prevIndex, pos + 1, memo);
+    memo[prevIndex + 1][pos] =  Math.max(taken, notTaken);
+    return memo[prevIndex + 1][pos];
+  }
+
+
+  /**
+   * Time complexity : O(n^2). Two loops of nnn are there.
+   * Space complexity : O(n). dp array of size n is used.
+   * @param arr
+   * @return
+   */
+  public int llnds(int[] arr) {
+    if (arr.length == 0) {
+      return 0;
+    }
     int[] lis = new int[arr.length];
     Arrays.fill(lis, 1);
 
     int max = 0;
-    for (int i = 0; i < arr.length; i++) {
+    for (int i = 1; i < arr.length; i++) {
       for (int j = 0; j < i; j++) {
         if (arr[j] <= arr[i]) {
           lis[i] = Math.max(lis[i], lis[j] + 1);
@@ -20,6 +78,8 @@ public class LongestNonDecreasingSubsequence {
   }
 
   public static void main(String[] args) {
-    System.out.println(new LongestNonDecreasingSubsequence().lnds(new int[]{10, 22, 9, 33, 21, 50, 41, 60}));
+    System.out.println(new LongestNonDecreasingSubsequence().llndsRec(new int[]{10, 22, 22, 22, 9, 33, 21, 50, 41, 60}));
+    System.out.println(new LongestNonDecreasingSubsequence().llndsMemo(new int[]{10, 22, 22, 22, 9, 33, 21, 50, 41, 60}));
+    System.out.println(new LongestNonDecreasingSubsequence().llnds(new int[]{10, 22, 22, 22, 9, 33, 21, 50, 41, 60}));
   }
 }
